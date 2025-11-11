df6fc0561befb91bb240f6b72e9a12679d3a72f5
Rama Bagus Prasetyo <contoh@rama.com>
2025-11-11 11:07:01 +0700

    Setup koneksi database MySQL (db.php)

--- db.php
+++ db.php
@@ -1,11 +1,10 @@
 <?php
+// Koneksi ke database SIMRS
 $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "simrs";
-
 $conn = mysqli_connect($host, $user, $pass, $db);
-
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
